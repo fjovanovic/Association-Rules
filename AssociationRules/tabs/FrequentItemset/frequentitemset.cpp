a558478a137950ef99d31c90887e2c040f8509ad
fjovanovicc <mi18336@alas.matf.bg.ac.rs>
2024-12-18 18:28:33 +0100

    Added implementation for constructor and getters

--- AssociationRules/tabs/FrequentItemset/frequentitemset.cpp
+++ AssociationRules/tabs/FrequentItemset/frequentitemset.cpp
@@ -1,3 +1,57 @@
 #include "frequentitemset.h"
 
-FrequentItemset::FrequentItemset() {}
+
+FrequentItemset::FrequentItemset()
+{
+    QString projectRoot = QCoreApplication::applicationDirPath() + "/../";
+    QString absoluteRootPath = QDir(projectRoot).absolutePath();
+
+    _inputOpenFilePath = absoluteRootPath + "/AssociationRules/resources/Frequent Itemset/";
+    _outputOpenFilePath = absoluteRootPath + "/AssociationRules/resources/Frequent Itemset/";
+    _inputFilePath = absoluteRootPath + "/AssociationRules/resources/Frequent Itemset/input4_100.txt";
+    _outputFilePath = absoluteRootPath + "/AssociationRules/resources/Frequent Itemset/output.txt";
+}
+
+
+QString FrequentItemset::getInputFilePath()
+{
+    return _inputFilePath;
+}
+
+
+QString FrequentItemset::getOutputFilePath()
+{
+    return _outputFilePath;
+}
+
+
+QString FrequentItemset::onBrowseButtonClicked()
+{
+    return "";
+    // TODO
+}
+
+
+QString FrequentItemset::onChangeButtonClicked()
+{
+    return "";
+    // TODO
+}
+
+
+void FrequentItemset::onRunAlgorithmButtonClicked(QGraphicsScene *scene)
+{
+    // TODO
+}
+
+
+void FrequentItemset::freqOnForwardButtonClicked()
+{
+    // TODO
+}
+
+
+void FrequentItemset::freqOnBackButtonClicked()
+{
+    // TODO
+}
--- AssociationRules/tabs/FrequentItemset/frequentitemset.h
+++ AssociationRules/tabs/FrequentItemset/frequentitemset.h
@@ -1,10 +1,34 @@
 #ifndef FREQUENTITEMSET_H
 #define FREQUENTITEMSET_H
 
+#include <QString>
+#include <QCoreApplication>
+#include <QFileDialog>
+#include <QGraphicsScene>
+
+
 class FrequentItemset
 {
 public:
+    // Constructors
     FrequentItemset();
+
+    // Getters
+    QString getInputFilePath();
+    QString getOutputFilePath();
+
+    // Slots
+    QString onBrowseButtonClicked();
+    QString onChangeButtonClicked();
+    void onRunAlgorithmButtonClicked(QGraphicsScene *scene);
+    void freqOnForwardButtonClicked();
+    void freqOnBackButtonClicked();
+
+private:
+    QString _inputFilePath;
+    QString _outputFilePath;
+    QString _inputOpenFilePath;
+    QString _outputOpenFilePath;
 };
 
 #endif // FREQUENTITEMSET_H
